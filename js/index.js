651b6c39f74ae3942701c60a4c180cbe9aa8283a
TARAALYSSA <tara.alyssathompson91@gmail.com>
2024-04-11 17:37:27 -0500

    js update
    adjusted javascript so submit button would be the button that trigger the modal

--- js/index.js
+++ js/index.js
@@ -2,3 +2,17 @@ console.log("Your index.js file is loaded correctly!");
 $(".Submitbutton").hover(function () {
   $(this).css("background-color", "red");
 });
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
